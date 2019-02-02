4e8f3f863f95251e1c1af8e32da595f5c2699241
Jonathan Perry <sequencemedia@sequencemedia.net>
2019-02-02 16:51:53 +0000

    `psList`

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ require('@babel/register')
 
 require('dotenv/config')
 
-const debug = require('debug')
-
 const {
   readFile
 } = require('sacred-fs')
+
+const debug = require('debug')
+
+const psList = require('ps-list')
 
 const {
   library,
@@ -20,10 +22,51 @@ const {
 
 const commander = require('commander')
 
+const NAME = 'il.App'
+process.title = NAME
+
 const app = async () => {
-  const error = debug('itunes-library:error')
+  let PACKAGE
+  try {
+    const s = await readFile('./package.json', 'utf8')
+    PACKAGE = JSON.parse(s)
+  } catch (e) {
+    const error = debug('itunes-library:process:error')
+
+    error(e)
+  }
 
   const {
+    name
+  } = PACKAGE
+
+  /**
+   *  Permit only one instance of the application
+   */
+  try {
+    const a = (await psList()).filter(({ name }) => name === NAME)
+    if (a.length > 1) {
+      const {
+        pid: PID
+      } = process
+
+      const {
+        pid
+      } = a.find(({ pid }) => pid !== PID)
+
+      throw new Error(`Application "${name}" has already started in process ${pid}.`)
+    }
+  } catch ({ message }) {
+    const error = debug('itunes-library:process:error')
+
+    error(message)
+    return
+  }
+
+  const log = debug('itunes-library:log')
+
+  const {
+    pid,
     argv,
     env: {
       JAR,
@@ -32,22 +75,20 @@ const app = async () => {
     }
   } = process
 
-  try {
-    const {
-      version
-    } = JSON.parse(await readFile('./package.json', 'utf8'))
+  log(`Starting application "${name}" in process ${pid}.`)
 
-    commander
-      .version(version)
-      .option('-j, --jar [jar]', 'Path to Saxon PE/EE JAR')
-      .option('-x, --xml [xml]', 'Path to iTunes Library XML')
-      .option('-d, --destination [destination]', 'Destination path for M3Us')
-      .option('-t, --tracks', 'Parse all tracks')
-      .option('-p, --playlists', 'Parse all playlists')
-      .parse(argv)
-  } catch (e) {
-    error(e)
-  }
+  const {
+    version
+  } = PACKAGE
+
+  commander
+    .version(version)
+    .option('-j, --jar [jar]', 'Path to Saxon PE/EE JAR')
+    .option('-x, --xml [xml]', 'Path to iTunes Library XML')
+    .option('-d, --destination [destination]', 'Destination path for M3Us')
+    .option('-t, --tracks', 'Parse all tracks')
+    .option('-p, --playlists', 'Parse all playlists')
+    .parse(argv)
 
   const {
     jar = JAR,
